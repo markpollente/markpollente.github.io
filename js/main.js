063582edafa274e63feb6e0a54262881b99a78f3
markpollente <5819641+markpollente@users.noreply.github.com>
2023-09-25 22:37:50 +0800

    Big changes

---
+++ js/main.js
@@ -0,0 +1,39 @@
+jQuery(document).ready(function($){
+	var isLateralNavAnimating = false;
+	
+	//open/close lateral navigation
+	$('.cd-nav-trigger').on('click', function(event){
+		event.preventDefault();
+		//stop if nav animation is running 
+		if( !isLateralNavAnimating ) {
+			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+			
+			$('body').toggleClass('navigation-is-open');
+			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				//animation is over
+				isLateralNavAnimating = false;
+			});
+		}
+	});
+});
+
+let bttButton = document.getElementById("bttButton");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    bttButton.style.display = "block";
+  } else {
+    bttButton.style.display = "none";
+  }
+}
+bttButton.addEventListener("click", btTop);
+function btTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
